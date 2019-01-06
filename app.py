0d4f0f6e9b4be949e82865b4d86c8d0b3f974e98
Parthiban Sudhaman <parthiban@Parthibans-MacBook-Pro.local>
2019-01-06 15:41:13 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+users = [
+    {
+        "name": "Nicholas",
+        "age": 42,
+        "occupation": "Network Engineer"
+    },
+    {
+        "name": "Elvin",
+        "age": 32,
+        "occupation": "Doctor"
+    },
+    {
+        "name": "Jass",
+        "age": 22,
+        "occupation": "Web Developer"
+    }
+]
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}, 200
+        
+class User(Resource):
+    def get(self, name):
+        for user in users:
+            if(name == user["name"]):
+                return user, 200
+        return "user not found", 404
+
+api.add_resource(HelloWorld, "/")
+api.add_resource(User, "/user/<string:name>")
+
+
+app.run(debug=True)
