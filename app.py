ab3cefced593219d94666b3ad464e4cc275bfa8b
Parthiban Sudhaman <parthiban@Parthibans-MacBook-Pro.local>
2019-01-06 17:40:34 +0530

    working copy

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
-from trail1.resources.user import User
+from resources.user import User
 
 app = Flask(__name__)
 api = Api(app)
