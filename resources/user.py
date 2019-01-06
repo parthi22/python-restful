4ae9deb807e4ec36b82a2a25cf455af80d5f5ffe
Parthiban Sudhaman <parthiban@Parthibans-MacBook-Pro.local>
2019-01-06 17:43:04 +0530

    working copy

---
+++ resources/user.py
@@ -0,0 +1,21 @@
+from flask_restful import Api, Resource, reqparse
+
+users = [
+    {
+        "name": "Nicholas",
+        "age": 42,
+        "occupation": "Network Engineer"
+    },
+    {
+        "name": "Elvin",
+        "age": 32,
+        "occupation": "Doctor"
+    }
+]
+
+class User(Resource):
+    def get(self, name):
+        for user in users:
+            if(name == user["name"]):
+                return user, 200
+        return "user not found", 404
